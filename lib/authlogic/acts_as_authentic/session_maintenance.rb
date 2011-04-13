05a75edf575df58ad7e02f4023ac85fda1969c9f
Seb <seb.augereau@gmail.com>
2011-04-13 12:25:04 +0200

    Remove RAILS 3 deprecation warnings

--- lib/authlogic/acts_as_authentic/session_maintenance.rb
+++ lib/authlogic/acts_as_authentic/session_maintenance.rb
@@ -70,7 +70,7 @@ module Authlogic
         # Save the record and skip session maintenance all together.
         def save_without_session_maintenance(*args)
           self.skip_session_maintenance = true
-          result = save(:validate => *args)
+          result = save(:validate => args.first)
           self.skip_session_maintenance = false
           result
         end
